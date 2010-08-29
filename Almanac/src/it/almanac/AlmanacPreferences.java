b946ea0f529f7b5175939479428d54c958ba3767
enrico.speranza <enrico.speranza@c8912858-210b-11de-ac9f-1d9250355bca>
2010-08-29 15:40:38 +0000

    - Aggiunto una prima versione molto rudimentale di gestione delle Preferences

---
+++ Almanac/src/it/almanac/AlmanacPreferences.java
@@ -0,0 +1,17 @@
+package it.almanac;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+
+public class AlmanacPreferences extends PreferenceActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		addPreferencesFromResource(R.xml.almanacpreference);
+	}
+	//TODO: Aggiungere lettura e scrittura e visulizzazione 
+	//altre info Almanac (Version, etc...)
+}
