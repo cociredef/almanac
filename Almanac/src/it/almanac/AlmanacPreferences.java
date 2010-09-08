38050995de16ac7fdf56b7602194344ec32afd62
enrico.speranza <enrico.speranza@c8912858-210b-11de-ac9f-1d9250355bca>
2010-09-08 20:27:14 +0000

    - Corretto errore nel db dei Santi - Mostra versione in Preference

--- Almanac/src/it/almanac/AlmanacPreferences.java
+++ Almanac/src/it/almanac/AlmanacPreferences.java
@@ -1,16 +1,28 @@
 package it.almanac;
 
-import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
 public class AlmanacPreferences extends PreferenceActivity {
+	private static final String TAG = "PreferenceActivity";
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		//Mostra Config
 		addPreferencesFromResource(R.xml.almanacpreference);
+		
+		String StrVersion;
+		try {
+	             PackageInfo pi = getPackageManager().getPackageInfo(getPackageName(), 0);
+	             StrVersion = pi.versionName;
+		     } catch (NameNotFoundException e) {
+	             Log.e(TAG, "Package name not found", e);
+	             StrVersion = "NOT FOUND!";
+		     }
+     	findPreference("Version").setSummary(StrVersion);
 	}
-	//TODO: Aggiungere lettura e scrittura e visulizzazione 
-	//altre info Almanac (Version, etc...)
 }
