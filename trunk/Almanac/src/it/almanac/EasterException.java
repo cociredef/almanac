83671d455a626003c19a9de610c0b53238708798
enrico.speranza <enrico.speranza@c8912858-210b-11de-ac9f-1d9250355bca>
2011-02-11 11:07:06 +0000

    0.00 lunedì 24 gennaio 2011
    - Pulizia Codice
    - Cancellazione classi non utili
    - Aggiunta Header GPLv3 od altro per alcuni file
    - Aggiunta img: web_hires_512.png

--- trunk/Almanac/src/it/almanac/EasterException.java
+++ trunk/Almanac/src/it/almanac/EasterException.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2008 Marco Ratto
+ *
+ * This file is part of the project EasterJ2ME.
+ *
+ * EasterJ2ME is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * EasterJ2ME is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EasterJ2ME; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 package it.almanac;
 
 import android.util.Log;
